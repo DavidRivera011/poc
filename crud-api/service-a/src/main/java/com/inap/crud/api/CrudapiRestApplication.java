f639aa4cc6382c9f9c944b39dba791caf6e87c94
DavidRivera011 <62643408+DavidRivera011@users.noreply.github.com>
2025-04-07 19:43:42 -0600

    proyecto base microprofile

---
+++ crud-api/service-a/src/main/java/com/inap/crud/api/CrudapiRestApplication.java
@@ -0,0 +1,11 @@
+package com.inap.crud.api;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class CrudapiRestApplication extends Application {
+}
